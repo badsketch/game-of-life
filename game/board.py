9768135c35900497510135bf42282a280bb743b3
Danielwang405 <danielwang405@gmail.com>
2020-04-05 00:48:56 -0400

    added ability to set new state

--- game/board.py
+++ game/board.py
@@ -15,3 +15,23 @@ class Board:
                 result += ' ' if j < self._ncols - 1 else ''
             result += '\n' if i < self._nrows - 1 else ''
         return result
+
+    def set_new_state(self, new_state):
+        if self._is_correct_shape(new_state):
+            for i in range(len(new_state)):
+                for j in range((len(new_state[0]))):
+                    if new_state[i][j]:
+                        self._board[i][j].live()
+                    else:
+                        self._board[i][j].die()
+        
+    # validation helper for setting new state
+    def _is_correct_shape(self, new_state):
+        if len(new_state) != self._nrows:
+            raise Exception('new state does not match number of rows')
+        
+        for row in range(len(new_state)):
+            if len(new_state[row]) != self._ncols:
+                raise Exception('new state does not match number of columns')
+        
+        return True
