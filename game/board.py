392baf5bc24ac7f89506bab7434e01c395d63cf7
Danielwang405 <danielwang405@gmail.com>
2020-04-07 21:13:34 -0400

    began board calculation implementation

--- game/board.py
+++ game/board.py
@@ -23,6 +23,28 @@ class Board:
             result += '\n' if i < self._nrows - 1 else ''
         return result
 
+    def calculate_next_state_for_index(self, i: int, j: int) -> bool:
+        neighbors = self.get_neighbors_for_index(i, j)
+        live_count = len([n.is_alive for n in neighbors if n])
+        if live_count < 2 or live_count > 3:
+            return False
+        else:
+            return True
+
+    # determines neighbors given index including edge cases
+    def get_neighbors_for_index(self, i: int, j: int) -> list:
+        return[
+            self._board[i - 1][j - 1],
+            self._board[i - 1][j],
+            self._board[i - 1][j + 1],
+            self._board[i][j - 1],
+            self._board[i][j + 1],
+            self._board[i + 1][j - 1],
+            self._board[i + 1][j],
+            self._board[i + 1][j + 1]
+        ]
+        
+
     def set_new_state(self, new_state: list) -> None:
         if self._is_correct_shape(new_state):
             for i in range(len(new_state)):
@@ -42,3 +64,11 @@ class Board:
                 raise Exception('new state does not match number of columns')
         
         return True
+
+    # TODO
+    def liven_at_index(self, row: int, col: int) -> None:
+        self._board[row][col].die()
+
+    # TODO
+    def kill_at_index(self, row: int, col: int) -> None:
+        self._board[row][col].live()
--- tests/test_board.py
+++ tests/test_board.py
@@ -40,7 +40,7 @@ class Test_Board(unittest.TestCase):
         new_state = [[0,0,0],[1,0,1]]
         with self.assertRaisesRegex(Exception, 'new state does not match number of rows'):
             board.set_new_state(new_state)
-        
+
     def test_sets_new_board_state_wrong_num_cols(self):
         board = Board(3,3)
         new_state_1 = [[0,0],[1,1],[1,1]]
@@ -54,4 +54,22 @@ class Test_Board(unittest.TestCase):
         new_state_3 = [[0,0,0],[1,1,1],[1,1]]
         with self.assertRaisesRegex(Exception, 'new state does not match number of columns'):
             board.set_new_state(new_state_3)
+
+    def test_calculates_next_board_stage(self):
+        seed_state = [[1,0,0],[0,1,0],[0,0,0]]
+        board = Board(3, 3, seed_state)
+        board.calculate_next_state_for_index(1,1)
+        next_state_output = '◼︎ ◻ ◻\n◻ ◻ ◻\n◻ ◻ ◻'
+        self.assertMultiLineEqual(str(board), next_state_output)
+
+    def test_get_neighbors_for_index_regular(self):
+        seed_state = [[1,0,0],[0,1,0],[0,0,0]]
+        board = Board(3,3,seed_state)
+        expected = board.get_neighbors_for_index(1,1)
+        neighbors = [1,0,0,0,0,0,0,0]
+        self.assertListEqual([n.is_alive for n in expected if n], neighbors)
+        
+
+
+        
 
